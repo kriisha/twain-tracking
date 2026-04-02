82bee50298c5103daff42157f4c789a53ce8d231
kriisha <jens.duerinckx.1@gmail.com>
2026-04-02 14:01:16 +0200

    Update resume.js

--- api/resume.js
+++ api/resume.js
@@ -13,7 +13,7 @@
 
 const { TOKEN, findContact, setCorsHeaders } = require('./_helpers');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   setCorsHeaders(req, res);
 
   if (req.method === 'OPTIONS') return res.status(200).end();
